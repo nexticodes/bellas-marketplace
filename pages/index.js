09b6acd45bcd14731c6630e378f36249f84cab77
Ernest Bruno <erndobruno@gmail.com>
2022-04-12 12:48:44 -0700

    Fix classname to className.

--- pages/index.js
+++ pages/index.js
@@ -87,7 +87,7 @@ export default function Home() {
         );
 
     return (
-        <div classname="flex justify-center">
+        <div className="flex justify-center">
             <div className="px-4" style={{ maxWidth: "1600px" }}>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
                     {nfts.map((nft, i) => (
